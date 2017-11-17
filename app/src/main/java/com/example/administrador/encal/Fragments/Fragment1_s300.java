79ad118bc94c9486d5b78cb13dc7e7ef12f97f10
Wilfredocb90 <33680900+Wilfredocb90@users.noreply.github.com>
2017-11-16 20:10:26 -0500

    vistas completas

---
+++ app/src/main/java/com/example/administrador/encal/Fragments/Fragment1_s300.java
@@ -0,0 +1,30 @@
+package com.example.administrador.encal.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrador.encal.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment1_s300 extends Fragment {
+
+
+    public Fragment1_s300() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment1_s300, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/administrador/encal/Pojos/PojoF3S100.java
@@ -0,0 +1,8 @@
+package com.example.administrador.encal.Pojos;
+
+/**
+ * Created by USUARIO on 16/11/2017.
+ */
+
+public class PojoF3S100 {
+}
